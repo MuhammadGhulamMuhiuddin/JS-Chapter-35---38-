074051a3337951b400099b6033fa29ba6f4ca2ed
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2024-10-20 14:07:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// // // Question No.5
+
+// // // var num = +prompt("Enter Your Number");
+// // // function saquareOfNumber(){
+// // //     var result = num * num;
+// // //     console.log(result);
+// // // } 
+// // // saquareOfNumber();
+
+
+// // // Question No.4 
+// var num1 = +prompt("Enter The First Number");
+// var num2 = +prompt("Enter The Second Number");
+// var operators = prompt("Enter The Operator like: + - * /");
+// var msg = "Thanks For Your Input! Now You Can Check Your Result In Console";
+// alert(msg);
+
+// function myCalculator(num1, num2, operators) {
+//     var result;
+
+//     switch (operators) {
+//         case '+':
+//             result = num1 + num2;
+//             break;
+
+//         case '-':
+//             result = num1 - num2;
+//             break;
+
+//         case '*':
+//             result = num1 * num2;
+//             break;
+
+//         case '/':
+//             if (num2 !== 0) {
+//                 result = num1 / num2;  
+            
+//             } else {
+//                 result = "Cannot divide by zero";
+//             }
+//             break;
+
+//         default:
+//             result = "Ops! Operation doesn't match. Try Again 😊";
+//     }
+
+//     return result;  
+// }
+
+// var mainResult = myCalculator(num1, num2, operators);
+// console.log("Result: " + mainResult);  
+ 
